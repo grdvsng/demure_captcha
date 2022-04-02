1e1e75b59ba548ce76e53011f094c0a735d31102
grdvsng <grdvsng@gmail.com>
2022-04-02 22:50:21 +0400

    update

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ import setuptools
 with open( "README.md", "r", encoding="utf-8" ) as fh:
     long_description = fh.read( )
 
+with open( "requirements.txt", "r", encoding="utf-8" ) as fh:
+    install_requires = [ x.replace( "==", ">=" ).strip( ) for x in fh.readlines( ) ]
+
+packages = setuptools.find_packages(where='./src')
+
 setuptools.setup(
     name                         = "demure_captcha",
     versio                       = "0.8.1",
@@ -11,14 +16,14 @@ setuptools.setup(
     description                  = "Simple but customize captcha generator( image + voice )",
     long_description             = long_description,
     long_description_content_type= "text/markdown",
-    url                          = "https://github.com/pypa/sampleproject",
-    project_urls                 = {
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
+    url                          = "https://github.com/grdvsng/demure_captcha",
+    project_urls                 = { },
+    classifiers                  = [
+        'Programming Language :: Python :: 3.8',
         "License :: OSI Approved :: MIT License",
     ],
-    package_dir={ "": "src" },
-    packages=setuptools.find_packages( where="src" ),
-    python_requires=">=3.8",
+    install_requires             = install_requires,
+    package_dir                  = { "": "src" },
+    packages                     = packages,
+    python_requires              = ">=3.8",
 )
